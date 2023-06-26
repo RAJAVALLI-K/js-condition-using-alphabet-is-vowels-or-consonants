2f082b737dca0f6bc880745a998acf6edae71321
RAJAVALLI-K <rajavalli1932bca@gmail.com>
2023-06-26 15:42:54 +0530

    add js-condition-using-alphabet-is-vowels-or-consonants

---
+++ main.js
@@ -0,0 +1,9 @@
+// 2.check if a given alphabet is vowels or consonants
+
+let value = prompt('enter a value:');
+if (value == "a" || value == "i" || value == "o" || value == "u" || value == "e" || value == "A" || value == "E" || value == "I" || value == "O" || value == "U") {
+    console.log("alphabet is vowels");
+}
+else {
+    console.log("alphabet is consonants");
+}
